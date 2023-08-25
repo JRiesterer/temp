4bd89c76557e3b7c759dec75cecfc4bed4fc8086
JRiesterer <105013121+JRiesterer@users.noreply.github.com>
2023-08-25 14:18:44 -0400

    trial upload code

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,58 @@
+
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$fname = "";
+	$lname = "";
+
+	$conn = new mysqli("localhost", "access", "sqlsqlsql2", "db1"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID,fname,lname FROM Users WHERE login=? AND password =?");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['fname'], $row['lname'], $row['ID'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"fname":"","lname":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $fname, $lname, $id )
+	{
+		$retValue = '{"id":' . $id . ',"fname":"' . $fname . '","lname":"' . $lname . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
